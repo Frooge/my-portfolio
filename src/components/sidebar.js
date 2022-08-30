ccb327945059b677ed200dc0d21d4141dd74bc28
Frooge <jaantaros@gmail.com>
2022-08-30 22:59:14 +0800

    installed packages and added sidebar

---
+++ src/components/sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './sidebar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faCode, faBook, faContactCard, faBirthdayCake } from '@fortawesome/free-solid-svg-icons';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="logo w-100 mb-5 mt-4">
+          <FontAwesomeIcon icon={faBirthdayCake}/>
+        </div>
+        <ul className="nav flex-column mb-auto text-center">
+          <li class="nav-item active">
+            <FontAwesomeIcon icon={faHouse}/>
+          </li>
+          <li class="nav-item">
+            <FontAwesomeIcon icon={faCode} />
+          </li>
+          <li class="nav-item">
+            <FontAwesomeIcon icon={faBook} />
+          </li>
+          <li class="nav-item">
+            <FontAwesomeIcon icon={faContactCard} />
+          </li>
+        </ul> 
+    </div>
+  )
+}
