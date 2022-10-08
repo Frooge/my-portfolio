cae0793139ada93ebea46ce40a3efd29687b662a
Frooge <jaantaros@gmail.com>
2022-10-09 00:08:59 +0800

    integrated emailjs

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import emailjs from '@emailjs/browser';
+
 
 import './contacts.scss';
 
 export default function Contacts() {
+  const [isLoading, setLoading] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.sendForm('service_ssovj9f', 'template_2l7t8dr', form.current, 'DHgBy285xZxFafVKH')
+      .then(() => {
+          alert('Message successfully sent!');
+          setLoading(false);
+      }, () => {
+        alert('Failed to send message.');
+        setLoading(false);
+      });
+  };
+
   return (
     <div id="contacts" className="contacts">
         <h1 align="center">- Contact Me -</h1>
         <AnimationOnScroll animateIn="animate__fadeIn" animateOnce="true" delay="0">
           <div className="contact-form">
-            <form className="row">
+            <form ref={form} className="row" onSubmit={sendEmail}>
             <div class="form-group col-6">
               <input type="text" class="form-control" name="name" placeholder="Name" required/>
             </div>
@@ -23,7 +42,7 @@ export default function Contacts() {
               <textarea rows={10} class="form-control" name="message" placeholder="Message" required/>
             </div>
             <div class="form-group d-flex justify-content-end">
-              <button type="submit" class="btn btn-warning col-3">Submit</button>
+              <button type="submit" class="btn btn-warning col-3" disabled={isLoading}>Submit</button>
             </div>
             </form>
           </div>
