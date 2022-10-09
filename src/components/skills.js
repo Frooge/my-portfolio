750dd91941b1e9c2e767b6d86e224fcf79984351
Frooge <jaantaros@gmail.com>
2022-10-10 00:22:11 +0800

    added profile

--- src/components/skills.js
+++ src/components/skills.js
@@ -7,8 +7,22 @@ import './skills.scss'
 export default function Skills() {
     return (
         <div id="skills" className="skills">
-            <h1 align="center">- My Skills && Experiences -</h1>
-            <div className="container">
+            <div className="container d-flex flex-column">
+                <div className="row align-items-center profile">
+                    <div className="col-3">
+                        <img className="dev-face" src="https://avatars.githubusercontent.com/u/88530893?s=400&u=c3c1e638fa48a121255cfb89507769a8b2ab1e7d&v=4" alt="dev_face"/>
+                    </div>
+                    <div className="col text-area">
+                        <p>
+                            Hey I'm Jade Andrie Rosales!
+                            I'm a 3rd year computer science student from the Univesity of San Carlos and an aspiring full-stack developer.
+                            I started my programming journey when I watched a documentary on how my favorite video games were made.
+                            It piqued my interest and started to learn python but soon realized that there are many things that you can learn and do with programming.
+                            That is how I got into this programming rabbit hole and found my passion in this field.
+                            As of now, I'm currently learning web development mainly with ReactJs.
+                        </p>
+                    </div>
+                </div>
                 <div className="row justify-content-center">
                     <AnimationOnScroll animateIn="animate__fadeIn" animateOnce="true" delay="0" className="col-content col-md-6">
                             <h2 align="center">Languages</h2>
