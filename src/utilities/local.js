e5aecc9f7e073a5a273ae6a55b6b63cdebdff80b
Frooge <jaantaros@gmail.com>
2022-10-16 10:39:39 +0800

    added new skills

--- src/utilities/local.js
+++ src/utilities/local.js
@@ -1,11 +1,14 @@
 export const local = {
     skills: {
+        // {name: "",ref: "", img: ""},
         languages: [
             {name: "c",ref: "https://www.cprogramming.com/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"},
+            {name: "cpp",ref: "https://www.w3schools.com/cpp/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"},
             {name: "csharp",ref: "https://www.w3schools.com/cs/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"},
+            {name: "java",ref: "https://www.java.com", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"},
+            {name: "python",ref: "https://www.python.org", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"},
             {name: "css3",ref: "https://www.w3schools.com/css/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"},
             {name: "html5",ref: "https://www.w3.org/html/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"},
-            {name: "java",ref: "https://www.java.com", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"},
             {name: "javascript",ref: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"},
             {name: "php",ref: "https://www.php.net", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"},
         ],
@@ -15,15 +18,22 @@ export const local = {
             {name: "express",ref: "https://expressjs.com", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"},
             {name: "laravel",ref: "https://laravel.com/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-plain-wordmark.svg"},
             {name: "react",ref: "https://reactjs.org/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"},
+            {name: "vue",ref: "https://vuejs.org/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg"},
             {name: "nodejs",ref: "https://nodejs.org", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"},
             {name: "xamarin",ref: "https://dotnet.microsoft.com/apps/xamarin", img: "https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/xamarin.svg"},
+            {name: "reactnative",ref: "https://reactnative.dev/", img: "https://reactnative.dev/img/header_logo.svg"},
         ],
         tools: [
             {name: "firebase",ref: "https://firebase.google.com/", img: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"},
             {name: "git",ref: "https://git-scm.com/", img: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"},
             {name: "mongodb",ref: "https://www.mongodb.com/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"},
             {name: "mysql",ref: "https://www.mysql.com/", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"},
+            {name: "mssql",ref: "https://www.microsoft.com/en-us/sql-server", img: "https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg"},
             {name: "postman",ref: "https://postman.com", img: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"},
+            {name: "arduino",ref: "https://www.arduino.cc/", img: "https://cdn.worldvectorlogo.com/logos/arduino-1.svg"},
+            {name: "figma",ref: "https://www.figma.com/", img: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg"},
+            {name: "photoshop",ref: "https://www.photoshop.com/en", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg"},
+            {name: "unity",ref: "https://unity.com/", img: "https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg"},
         ],
     },
     projects: [
