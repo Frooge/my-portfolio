d1774e828d263c09ff9601f86eeb86087f944d62
Frooge <jaantaros@gmail.com>
2022-10-16 11:05:51 +0800

    added contact info

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -27,25 +27,40 @@ export default function Contacts() {
     <div id="contacts" className="contacts">
         <h1 align="center">Contact Me</h1>
         <AnimationOnScroll animateIn="animate__fadeIn" animateOnce="true" delay="0">
-          <div className="contact-form">
-            <form ref={form} className="row" onSubmit={sendEmail}>
-            <div class="form-group col-6">
-              <input type="text" class="form-control" name="name" placeholder="Name" required/>
+          <div className="contact-modal">
+            <div className="contact-details col-sm-4">
+              <h2>Contact Info</h2>
+              <ul>
+                <li>Email: <u>jaantaros@gmail.com</u></li>
+                <li>LinkedIn: <u>Jade Andrie Rosales</u></li>
+                <li>Discord: <u>Nter#7661</u></li>
+                <li>Facebook: <u>Jade Rosales</u></li>
+                <li>Twitter: <u>N/A</u></li>
+                <li>Instagram: <u>N/A</u></li>
+                <li>Phone#: <u>N/A</u></li>
+              </ul>
             </div>
-            <div class="form-group col-6">
-              <input type="text" class="form-control" name="email" placeholder="Email" required/>
+            <div className="contact-form col-sm-8">
+              <form ref={form} className="row" onSubmit={sendEmail}>
+              <div class="form-group col-6">
+                <input type="text" class="form-control" name="name" placeholder="Name" required/>
+              </div>
+              <div class="form-group col-6">
+                <input type="text" class="form-control" name="email" placeholder="Email" required/>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" placeholder="Subject" required/>
+              </div>
+              <div class="form-group">
+                <textarea rows={10} class="form-control" name="message" placeholder="Message" required/>
+              </div>
+              <div class="form-group d-flex justify-content-end">
+                <button type="submit" class="btn btn-warning col-3" disabled={isLoading}>Submit</button>
+              </div>
+              </form>
             </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="subject" placeholder="Subject" required/>
-            </div>
-            <div class="form-group">
-              <textarea rows={10} class="form-control" name="message" placeholder="Message" required/>
-            </div>
-            <div class="form-group d-flex justify-content-end">
-              <button type="submit" class="btn btn-warning col-3" disabled={isLoading}>Submit</button>
-            </div>
-            </form>
           </div>
+          
         </AnimationOnScroll>
     </div>
   )
