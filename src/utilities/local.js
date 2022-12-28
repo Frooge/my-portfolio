f3a914b666e91a7b24aee0d245cfa139f34513b7
Jade Andrie Rosales <88530893+Frooge@users.noreply.github.com>
2022-12-28 23:28:31 +0800

    updated projects

--- src/utilities/local.js
+++ src/utilities/local.js
@@ -40,6 +40,7 @@ export const local = {
     },
     projects: [
         // {img: , title: , desc: , year: , ref: },
+        {img: 'https://i.ibb.co/6HW9cHk/thread-app.png', title: 'Thread App', desc: 'An anonymous chat app implemented with Flutter and Firebase', year: '2022', ref: 'https://github.com/Frooge/thread_app'},
         {img: 'https://i.ibb.co/2888Mgm/budiga-home.png', title: 'Budiga App', desc: 'An inventory management system implemented with .NET framework', year: '2022', ref: 'https://github.com/therealmai/budiga_app'},
         {img: 'https://i.ibb.co/0CWncq7/signtitiktitle.png', title: 'Signtitik', desc: 'Sign language android app implemented with Xamarin', year: '2022', ref: 'https://github.com/ydblank/Signtitik'},
         {img: 'https://i.ibb.co/fFnJJsy/mcford.png' , title: 'Mcford Ecommerce Webapp', desc: 'An ecommerce website implemented with ReactJs and PHP', year: '2021', ref: 'https://github.com/Frooge/2104-ecommerce-webapp'},
